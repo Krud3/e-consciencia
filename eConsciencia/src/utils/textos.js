42b275952a107c51476eb12cd781adb25936c8c4
AndresCaHenao73 <andrescamilohenaohidalgo.steam@gmail.com>
2024-11-26 11:03:40 -0500

    centrar texto y acomodar carteles

---
+++ eConsciencia/src/utils/textos.js
@@ -0,0 +1,94 @@
+const concienciaAguaText = [
+    {
+      title: "The Water Crisis: A Global Problem",
+      txt: "Water is vital, but its scarcity is a growing problem. Climate change, overpopulation, and pollution are affecting its availability. It is urgent to raise awareness about responsible usage to ensure access in the future.",
+    },
+    {
+      title: "Every Drop Counts",
+      txt: "Every drop counts. Small changes, like turning off the tap while brushing your teeth or shortening your shower time, help save water.",
+    },
+    {
+      title: "Drinking Water is Scarce",
+      txt: "Drinking water is scarce. Although 70% of the Earth is covered by water, only 1% is accessible for human consumption.",
+    },
+    {
+      title: "Taking Action Against Water Scarcity",
+      txt: "Water scarcity affects millions. Reducing waste and supporting sustainable solutions is key to tackling this global challenge.",
+    },
+  ];
+  
+  const concienciaAguaitems = [
+    {
+      position: [4.3, 5, -7.5],
+      text: "shortage",
+      color: "blue",
+      rotation: [0, -Math.PI * 0.13, 0],
+    },
+    {
+      position: [8, 5, 6],
+      text: "drop",
+      color: "white",
+      rotation: [0, -Math.PI * 0.75, 0],
+    },
+    {
+      position: [-5.5, 5, 12],
+      text: "Scanty",
+      color: "blue",
+      rotation: [0, Math.PI * 0.74, 0],
+    },
+    {
+      position: [-11.5, 5, -1],
+      text: "impact",
+      color: "white",
+      rotation: [0, Math.PI * 0.47, 0],
+    },
+  ];
+  
+  const solucionesAguaText = [
+    {
+      title: "Uso Eficiente del Agua",
+      txt: "El uso eficiente del agua comienza en casa. Implementar sistemas de recolección de agua lluvia, usar electrodomésticos de bajo consumo y optimizar los riegos en jardines son prácticas clave para ahorrar agua.",
+    },
+    {
+      title: "Tecnología para la Conservación del Agua",
+      txt: "Innovaciones tecnológicas como los sistemas de desalinización, la reutilización de aguas grises y los filtros avanzados ofrecen soluciones sostenibles para garantizar el acceso al agua potable.",
+    },
+    {
+      title: "Agricultura Sostenible",
+      txt: "La agricultura sostenible juega un papel crucial. Técnicas como el riego por goteo, la rotación de cultivos y el uso de plantas resistentes a la sequía pueden reducir el consumo de agua en la agricultura.",
+    },
+    {
+      title: "Educación y Conciencia Social",
+      txt: "Fomentar la educación sobre la importancia del agua y sus soluciones es esencial. La sensibilización sobre las prácticas responsables de uso y conservación del agua puede generar un cambio a gran escala.",
+    },
+  ];
+  
+  const solutionAguaItems = [
+    {
+      position: [4.3, 5, -7.5],
+      text: "Efficient",
+      color: "blue",
+      rotation: [0, -Math.PI * 0.13, 0],
+    },
+    {
+      position: [8, 5, 6],
+      text: "Care",
+      color: "white",
+      rotation: [0, -Math.PI * 0.75, 0],
+    },
+    {
+      position: [-5, 5, 12],
+      text: "Sustainable",
+      color: "blue",
+      rotation: [0, Math.PI * 0.74, 0],
+    },
+    {
+      position: [-11.5, 5, -1],
+      text: "Education",
+      color: "white",
+      rotation: [0, Math.PI * 0.47, 0],
+    },
+  ];
+  
+   
+  export { concienciaAguaText, concienciaAguaitems, solucionesAguaText, solutionAguaItems };
