53cca5ded1c52d4cc6de15083b5d13b111480058
Juan Molina <juan.sebastian.molina@correounivalle.edu.co>
2024-12-10 23:51:39 -0500

    added shark movemnt also animation to tractor

--- eConsciencia/src/pages/contamination/Shark.jsx
+++ eConsciencia/src/pages/contamination/Shark.jsx
@@ -1,21 +1,28 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { useGLTF, useAnimations } from '@react-three/drei'
-import { RigidBody } from "@react-three/rapier";
 import * as THREE from 'three'; 
+import { useFrame } from '@react-three/fiber'
+import { RigidBody } from "@react-three/rapier";
 
 export function Shark(props) {
   const group = useRef()
   const { nodes, materials, animations } = useGLTF('/3d-models-contamination/shark-animated.glb')
+  const { actions } = useAnimations(animations, group)
 
-  const { actions } = animations ? useAnimations(animations, group) : {}
+  // Focos:
+  const F1 = new THREE.Vector3(1, -2.899, 54.539)
+  const F2 = new THREE.Vector3(40, -2.899, 54.539)
 
-  useEffect(() => {
-    if (actions && Object.keys(actions).length > 0) {
-      console.log('Acciones de animación disponibles:', actions)
-    } else {
-      console.log('No se encontraron acciones de animación.')
-    }
-  }, [actions])
+  // Centro de la elipse:
+  const M = F1.clone().add(F2).multiplyScalar(0.5) 
+
+  const c = F1.distanceTo(F2) / 2 
+  const a = 20 
+  const b = Math.sqrt(a*a - c*c) 
+
+
+  const [theta, setTheta] = useState(0)
+  const angularSpeed = 0.4
 
   useEffect(() => {
     if (actions && Object.keys(actions).length > 0) {
@@ -27,10 +34,30 @@ export function Shark(props) {
         action.setEffectiveTimeScale(1) 
         action.play()
       } else {
-        console.warn(`La animación '${nombreAnimacion}' no se encontró en las acciones disponibles.`)
+        console.warn(`La animación '${nombreAnimacion}' no se encontró.`)
       }
     }
   }, [actions])
+
+  useFrame((state, delta) => {
+    if (!group.current) return
+
+    const newTheta = theta + delta * angularSpeed
+    setTheta(newTheta)
+
+    // Posición en la elipse
+    const x = M.x + a * Math.cos(newTheta)
+    const z = M.z + b * Math.sin(newTheta)
+    group.current.position.set(x, M.y, z)
+
+
+    const aheadTheta = newTheta + 0.01
+    const xAhead = M.x + a * Math.cos(aheadTheta)
+    const zAhead = M.z + b * Math.sin(aheadTheta)
+
+    const lookTarget = new THREE.Vector3(xAhead, M.y, zAhead)
+    group.current.lookAt(lookTarget)
+  })
 
   return (
     <RigidBody type='fixed'>
